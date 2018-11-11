6f20c26c7eb181647fa3a7e2d9dbd07969303ac1
Hubert Cross <hcross@casacross.com.ni>
2018-11-10 23:08:05 -0600

    cleaning

--- c/huffmanCoding/huff.c
+++ c/huffmanCoding/huff.c
@@ -13,22 +13,14 @@
 int main(int argc, char *argv[]) {
 
 	char myString[] = "HUBERTEMMAOTIEMMA";
-	char c = 'A';
 	char array[255];
 
-	// printf("ugh: %i", c);
-	// array[255] = 'X';
-	// printf("\n");
-
-	// printf("ugh: %c", array['A']);
 	printf("\n");
 	printf("%s\n", myString);
 	printf("\n");
 
 	int x;
-
 	memset(&array, 0, 255);
-
 	int uniqueCharacters = 0;
 
 	// first store frequency of each character using its ascii value as an array index
@@ -71,15 +63,11 @@ int main(int argc, char *argv[]) {
 
 	// check heap contents
 	int i;
-	// btree_node_t *newBTNode;
 	for (i = 0; i < currentLength; i++) {
-		// newBTNode = (btree_node_t *) heap[i].something;
 		printf("Heap array node %i has key: %i ... element: %i  \n\n", i, heap[i].key, heap[i].element);
 	}
 
-	// return 0;
-	// ugh = heapRemoveMin(heap, &currentLength);
-	// printf("WTF: %c\n", ugh->element );
+
 
 	int frequency1, frequency2;
 	heap_node_t *t1, *t2, *tmp;
@@ -106,31 +94,8 @@ int main(int argc, char *argv[]) {
 		printf("Heap array node %i has key: %d ... element: %c ... \n\n", i, heap[i].key, heap[i].element);
 	}	
 
-	// return 0;
 
-	printf("ugh: %p\n", heap);
-	// newBTNode = heap[0].something;
-	printf("ugh: %i\n", heap->left->left->left->key);
-	printf("ugh: %c\n", heap->left->left->element);
-
-	// printf("ugh: %i\n", newBTNode->left->element);
-	// printf("ugh: %p\n", heap[0].something->right);
 
 	printNode(heap, 0);
-
-	//now we have the priority queue populated with single node binary trees that contain the characters in the original string
-	//the frequencies of these characters are the keys of the priorioty queue
-
-
-	// heap_node_t *heapArray = malloc(sizeof(heap_node_t));
-	// int currentLength;
-
-	// nodePtr = nodeArray;
-	// for (x = 0; x < uniqueCharacters; x++) {
-	// 	heapInsert(heapArray, &currentLength, nodePtr->key, nodePtr->element, nodePtr->something);
-	// 	// printf("node key: %c | element: %d", nodePtr->key, nodePtr->element);
-	// 	// printf("\n");
-	// 	nodePtr++;
-	// }
 
 }
